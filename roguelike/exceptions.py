f25ebff88d4a7aee5ad1676393c3dbae445819d4
Jay Kumogata <jay-kumogata@users.noreply.github.com>
2024-05-19 09:12:18 +0900

    Delete roguelike directory

--- roguelike/exceptions.py
+++
@@ -1,5 +0,0 @@
-class Impossible(Exception):
-    """Exception raised when an action is impossible to be performed.
-
-    The reason is given as the exception message.
-    """
--- roguelike/render_order.py
+++
@@ -1,9 +0,0 @@
-from enum import auto, Enum
-
-
-class RenderOrder(Enum):
-    CORPSE = auto()
-    ITEM = auto()
-    ACTOR = auto()
-
-# end of render_order.py    
