2ed996452e358928afdf0c757b7aab99fbf89e06
Jay Kumogata <jay-kumogata@users.noreply.github.com>
2024-05-26 10:51:36 +0900

    Add files via upload

---
+++ roguelike/exceptions.py
@@ -0,0 +1,8 @@
+class Impossible(Exception):
+    """Exception raised when an action is impossible to be performed.
+
+    The reason is given as the exception message.
+    """
+
+class QuitWithoutSaving(SystemExit):
+    """Can be raised to exit the game without automatically saving."""    
