11094adbc1e03c353a5dace53d40ac1fd0fff7b3
Jay Kumogata <jay-kumogata@users.noreply.github.com>
2025-03-12 20:33:28 +0900

    Add files via upload

--- pyxel/amabie/amabie.py
+++ pyxel/amabie/amabie.py
@@ -1,8 +1,29 @@
+###########################################
+# 
+# Amabie(*1) for Pyxel/Python
+#
+# Move amabie left and right with LEFT/RIGHT
+# Press DOWN to drop a ofuda(*2) and  
+# release DOWN to rupture it. Defeat 
+# incoming plagues they land in Japan.
+#
+# (*1) https://en.wikipedia.org/wiki/Amabie
+# (*2) https://en.wikipedia.org/wiki/Ofuda
+#
+# Mar 10, 2021 ver.1 changed characters and titles
+# Mar 11, 2021 ver.2 changed small
+# Mar 15, 2021 ver.3 translated into English
+# Mar 20, 2021 ver.4 changed characters and titles
+# Mar 11, 2025 ver.5 converted to Pyxel/Python with Grok3
+# Mar 12, 2025 ver.6 debug and refactor
+#
+###########################################
+
 import pyxel
 import random
 
 # Pyxelの初期化
-pyxel.init(64, 32, title="Amabie", fps=5)
+pyxel.init(64, 32, title="Amabie", fps=10)
 
 # リソース（スプライト）の定義
 pyxel.images[0].set(0, 0, [
@@ -10,21 +31,21 @@ pyxel.images[0].set(0, 0, [
     "77700770",
 ])
 pyxel.images[0].set(8, 0, [
-    "07777770",  # boat (Amabie)
+    "07777770",  # Amabie
     "77777777",
     "77077077",
     "77700777",
     "70077007",
 ])
 pyxel.images[0].set(16, 0, [
-    "70777000",  # boat-flip
+    "70777000",  # tomb
     "00707070",
     "00707000",
     "07777700",
     "77777770",
 ])
 pyxel.images[0].set(24, 0, [
-    "00007000",  # bomb (Ofuda)
+    "00007000",  # Ofuda
     "00070700",
     "00070700",
     "00707000",
@@ -32,19 +53,19 @@ pyxel.images[0].set(24, 0, [
     "00070000",
 ])
 pyxel.images[0].set(32, 0, [
-    "00707000",  # squid-0 (plague-0)
+    "00707000",  # plague-0
     "00770770",
     "07707700",
     "00070700",
 ])
 pyxel.images[0].set(40, 0, [
-    "00070700",  # squid-1 (plague-1)
+    "00070700",  # plague-1
     "07707700",
     "00770770",
     "00707000",
 ])
 pyxel.images[0].set(48, 0, [
-    "70070707",  # boom (Enmaku)
+    "70070707",  # Enmaku
     "00700070",
     "00707070",
     "07070700",
@@ -52,6 +73,7 @@ pyxel.images[0].set(48, 0, [
     "00707000",
     "70000070",
 ])
+
 # タイトルスプライト（AMABIE）
 pyxel.images[0].set(56, 0, [
     "07770007",  # title-0 (AM)
@@ -89,17 +111,16 @@ pyxel.images[0].set(80, 0, [
     "07707700",
     "07707777",
 ])
+
 # 落下アニメーション用スプライト（fall）
 pyxel.images[0].set(88, 0, [
-    "00000000",  # fall (falling Amabie)
+    "00000000",  # falling Amabie
     "70077007",
     "77700777",
     "77077077",
     "77777777",
     "07777770",
 ])
-
-#pyxel.save("amabie.pyxres")
 
 # ゲームの状態を管理するクラス
 class Game:
@@ -136,29 +157,28 @@ class Game:
         pyxel.blt(24, 12, 0, 64, 0, 8, 7)  # MA
         pyxel.blt(32, 12, 0, 72, 0, 8, 7)  # AB
         pyxel.blt(40, 12, 0, 80, 0, 8, 7)  # IE
-        #pyxel.text(16, 24, "Press SPACE to start", 7)
 
     def update_player(self):
         old_x = self.player_x
-        if pyxel.btn(pyxel.KEY_A) or pyxel.btn(pyxel.KEY_LEFT):  # Aキーで左移動
+        if pyxel.btn(pyxel.KEY_LEFT):   # LEFTキーで左移動
             self.player_x -= 3
-        if pyxel.btn(pyxel.KEY_D)or pyxel.btn(pyxel.KEY_RIGHT):  # Dキーで右移動
+        if pyxel.btn(pyxel.KEY_RIGHT):  # RIGHTキーで右移動
             self.player_x += 3
         if self.player_x < 0:
             self.player_x = 0
         if self.player_x > 56:
             self.player_x = 56
-        if (pyxel.btnp(pyxel.KEY_S) or pyxel.btnp(pyxel.KEY_DOWN)) and self.bomb_state == 0:  # Sキーでボム発射
+        if pyxel.btn(pyxel.KEY_DOWN) and self.bomb_state == 0:  # DOWNキーでボム発射
             self.bomb_x = self.player_x
             self.bomb_y = 10
             self.bomb_state = 1
 
     def update_bomb(self):
-        if self.bomb_state == 1:  # 落下中
+        if self.bomb_state == 1:     # 落下中
             self.bomb_y += 1
-            if self.bomb_y >= 25 or not (pyxel.btn(pyxel.KEY_S) or pyxel.btn(pyxel.KEY_DOWN)):
+            if self.bomb_y >= 25 or not pyxel.btn(pyxel.KEY_DOWN): 
                 self.bomb_state = 2  # 爆発開始
-        elif self.bomb_state >= 2:  # 爆発中
+        elif self.bomb_state >= 2:   # 爆発中
             self.bomb_state += 1
             if self.bomb_state >= 4:
                 self.bomb_state = 0  # 爆発終了
@@ -169,20 +189,21 @@ class Game:
 
     def update_enemies(self):
         self.enemy_timer += 1
-        if self.enemy_timer > 30:
+        if self.enemy_timer > 40:
             self.spawn_enemy()
-            self.enemy_timer = random.randint(10, 30)
+            self.enemy_timer = random.randint(10, 20)
 
-        for enemy in self.enemies[:]:
-            enemy[1] -= 1
-            if enemy[1] <= 8:
-                self.game_over = True
-                self.state = "sinking"  # 落下アニメーションへ移行
-            if (self.bomb_state >= 2 and 
-                abs(enemy[0] - self.bomb_x) < 8 and 
-                abs(enemy[1] - self.bomb_y) < 8):
-                self.enemies.remove(enemy)
-                self.score += 1
+        if self.enemy_timer % 2 == 0:
+            for enemy in self.enemies[:]:
+                enemy[1] -= 1
+                if enemy[1] <= 8:
+                    self.game_over = True
+                    self.state = "sinking"  # 落下アニメーションへ移行
+                if (self.bomb_state >= 2 and 
+                    abs(enemy[0] - self.bomb_x) < 8 and 
+                    abs(enemy[1] - self.bomb_y) < 8):
+                    self.enemies.remove(enemy)
+                    self.score += 1
 
     def update_sinking(self):
         self.sink_timer += 1
@@ -208,7 +229,7 @@ class Game:
             self.draw_score()
         elif self.state == "sinking":
             pyxel.cls(0)
-            pyxel.blt(self.player_x, self.player_y, 0, 16, 0, 8, 5)  # お墓描画
+            pyxel.blt(self.player_x, self.player_y, 0, 16, 0, 8, 5)  # 墓描画
             self.draw_waves()
             pyxel.blt(self.player_x, self.sink_y, 0, 88, 0, 8, 6)  # 落下中のアマビエ
         elif self.state == "game_over":
@@ -219,7 +240,7 @@ class Game:
 
     def update(self):
         if self.state == "title":
-            if pyxel.btnp(pyxel.KEY_SPACE):  # スペースキーでゲーム開始
+            if pyxel.btnp(pyxel.KEY_DOWN):  # DOWNキーでゲーム開始
                 self.state = "playing"
                 self.reset()
         elif self.state == "playing":
@@ -229,7 +250,7 @@ class Game:
         elif self.state == "sinking":
             self.update_sinking()
         elif self.state == "game_over":
-            if pyxel.btnp(pyxel.KEY_SPACE):  # スペースキーでリスタート
+            if pyxel.btnp(pyxel.KEY_DOWN):  # DOWNキーでリスタート
                 self.state = "title"
         if pyxel.btnp(pyxel.KEY_Q):  # Qキーで終了
             pyxel.quit()
@@ -245,3 +266,4 @@ def draw():
 
 pyxel.run(update, draw)
 
+# End of amabie.py
