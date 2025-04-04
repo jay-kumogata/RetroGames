e00e52431f5395a2d19249cf7368e780bb4eda32
Jay Kumogata <jay-kumogata@users.noreply.github.com>
2025-04-04 21:35:40 +0900

    Add files via upload

--- pyxel/lander/README.md
+++ pyxel/lander/README.md
@@ -9,7 +9,7 @@ This is a game where you operate a lunar module and land on the moon.
 Lunar Lander (Atari / 1979) is a famous implementation, 
 but the Apollo program's lunar lander simulator seems to be the original. 
 
-![](https://github.com/jay-kumogata/RetroGames/blob/main/pyxel/lander/screenshots/lander01.gif)
+![](https://github.com/jay-kumogata/RetroGames/blob/main/pyxel/lander/screenshots/lander02.gif)
 
 ## How to Play
 
@@ -20,6 +20,6 @@ Or you can play [here](https://kitao.github.io/pyxel/wasm/launcher/?run=jay-kumo
 
 ## How to Controll
 
-Use the [W] / [A] / [D] key or [up-arrow] / [left-arrow] / [right-arrow] key to move up / left / right.  
+Use the [up-arrow] / [left-arrow] / [right-arrow] key to move up / left / right.  
 Operate the lunar module and land on the surface of the moon.
 
--- pyxel/lander/lander.py
+++ pyxel/lander/lander.py
@@ -1,12 +1,30 @@
+# Lunar lander clone for the CHIP-8
+#
+# Coded by Jeffrey Massung as an example of the
+# CHIP-8 assembler.
+#
+# Have fun!
+#---------------------------------------------------------------------------
+# Note: this source has been ported to Octo by Jay Kumogata on 05 Dec 2021.
+#       (only the notation changed: it has been converted manually.)
+#       And has been converted to Pyxel/Python with Grok3(beta) on 12 Mar 2025.
+
 import pyxel
-import random
+import random as rnd
 
 class LunarLander:
     def __init__(self):
         pyxel.init(128, 64, title="Lunar Lander")
-        pyxel.sound(0).set("c3", "s", "7", "f", 5)   # 推力音
-        pyxel.sound(1).set("e2", "n", "5", "f", 20)  # 爆発音
+        pyxel.sounds[0].set("c3", "s", "7", "f", 5)   # 推力音
+        pyxel.sounds[1].set("e2", "n", "5", "f", 20)  # 爆発音
 
+        # 乱択パレット
+        #self.pal = [1,6,8,7,9,10,5,3]
+        self.pal = [1,rnd.randint(6,7),rnd.randint(10,11),rnd.randint(14,15),\
+                    rnd.randint(8,9),rnd.randint(12,13),rnd.randint(4,5),\
+                    rnd.randint(2,3)]
+         
+        
         # 変数初期化
         self.x = 64
         self.y = 0
@@ -31,9 +49,9 @@ class LunarLander:
 
     def init_level(self):
         self.level += 1
-        self.x = random.randint(32, 96)
+        self.x = rnd.randint(32, 96)
         self.y = 0
-        self.vx = random.randint(-15, 15)
+        self.vx = rnd.randint(-15, 15)
         self.vy = 8
         self.thrust = 0x80
         self.tilt = 0x80
@@ -46,10 +64,10 @@ class LunarLander:
         self.init_platform()
 
     def init_ground(self):
-        elev = random.randint(20, 40)
+        elev = rnd.randint(20, 40)
         self.elevation = [elev] * 15
         for i in range(3):
-            delta = random.randint(-8, 8)
+            delta = rnd.randint(-8, 8)
             for j in range(5):
                 idx = i * 5 + j
                 if idx < 15:
@@ -59,7 +77,7 @@ class LunarLander:
     def init_platform(self):
         lowest = min(self.elevation)
         candidates = [i for i, e in enumerate(self.elevation) if e == lowest]
-        self.plat_x = random.choice(candidates) * 8
+        self.plat_x = rnd.choice(candidates) * 8
 
     def update(self):
         if self.game_over:
@@ -128,29 +146,29 @@ class LunarLander:
                 pyxel.play(1, 1)
 
     def draw(self):
-        pyxel.cls(0)
+        pyxel.cls(self.pal[0])
 
         # 格子状の地形描画
         for i, elev in enumerate(self.elevation):
             if elev % 2 == 1: elev -= 1
             for y in range(64 - elev, 64, 2):
                 for x in range(i * 8, i * 8 + 8, 2):
-                    pyxel.pset(x, y, 6)
+                    pyxel.pset(x, y, self.pal[1])
 
         # プラットフォーム描画
-        pyxel.rect(self.plat_x, 64 - self.elevation[self.plat_x // 8], 8, 1, 8)
+        pyxel.rect(self.plat_x, 64 - self.elevation[self.plat_x // 8], 8, 1, self.pal[2])
 
         # 着陸船描画
         if not self.game_over or self.explosion_frame == -1:
             for y, row in enumerate([0b01110000, 0b11111000, 0b11111000, 0b01010000, 0b11011000]):
                 for x in range(8):
                     if row & (1 << (7 - x)):
-                        pyxel.pset(self.x + x, self.y + y, 7)
+                        pyxel.pset(self.x + x, self.y + y, self.pal[3])
             if self.thrust_visible and self.fuel > 0:
                 for y, row in enumerate([0b01010000, 0b00100000, 0b00100000]):
                     for x in range(8):
                         if row & (1 << (7 - x)):
-                            pyxel.pset(self.x + x, self.y + 5 + y, 9)
+                            pyxel.pset(self.x + x, self.y + 5 + y, self.pal[4])
 
         # 爆発アニメーション
         if self.explosion_frame >= 0:
@@ -164,125 +182,26 @@ class LunarLander:
             for y, row in enumerate(explosion_data[self.explosion_frame]):
                 for x in range(5):
                     if row & (1 << (4 - x)):
-                        pyxel.pset(self.x + x + 1, self.y + y, 10)
-
+                        pyxel.pset(self.x + x + 1, self.y + y, self.pal[5])
+                    
         # 燃料ゲージ
-        pyxel.rect(120, 0, 8, 64, 5)  # 枠
-        pyxel.rect(121, 64 - self.fuel, 6, self.fuel, 3)  # 燃料バー
-        self.draw_vertical_text(122, 2, "FUEL", 7)  # 「FUEL」を縦書き
+        pyxel.rect(120, 0, 8, 64, self.pal[6])  # 枠
+        pyxel.rect(121, 64 - self.fuel, 6, self.fuel, self.pal[7])  # 燃料バー
+        self.draw_vertical_text(122, 2, "FUEL", self.pal[3])  # 「FUEL」を縦書き
 
         # スコアとレベル
-        self.draw_text(2, 2, f"LEVEL {self.level}", 7)
-        self.draw_text(2, 10, f"SCORE {self.score}", 7)
+        pyxel.text(2, 2, f"LEVEL {self.level}", self.pal[3])
+        pyxel.text(2, 10, f"SCORE {self.score}", self.pal[3])
 
         if self.game_over and self.explosion_frame >= 4:
-            self.draw_text(40, 25, "GAME OVER", 8)
-
-    # 横書きテキスト描画（アルファベット＋数字）
-    def draw_text(self, x, y, text, col):
-        font = [
-            [0b01100, 0b10010, 0b11110, 0b10010, 0b10010],  # A
-            [0b11100, 0b10010, 0b11100, 0b10010, 0b11100],  # B
-            [0b01100, 0b10000, 0b10000, 0b10000, 0b01100],  # C
-            [0b11100, 0b10010, 0b10010, 0b10010, 0b11100],  # D
-            [0b11110, 0b10000, 0b11100, 0b10000, 0b11110],  # E
-            [0b11110, 0b10000, 0b11100, 0b10000, 0b10000],  # F
-            [0b01100, 0b10000, 0b10110, 0b10010, 0b01100],  # G
-            [0b10010, 0b10010, 0b11110, 0b10010, 0b10010],  # H
-            [0b11110, 0b00100, 0b00100, 0b00100, 0b11110],  # I
-            [0b00110, 0b00010, 0b00010, 0b10010, 0b01100],  # J
-            [0b10010, 0b10100, 0b11000, 0b10100, 0b10010],  # K
-            [0b10000, 0b10000, 0b10000, 0b10000, 0b11110],  # L
-            [0b10010, 0b11010, 0b10110, 0b10010, 0b10010],  # M
-            [0b10010, 0b11010, 0b10110, 0b10010, 0b10010],  # N
-            [0b01100, 0b10010, 0b10010, 0b10010, 0b01100],  # O
-            [0b11100, 0b10010, 0b11100, 0b10000, 0b10000],  # P
-            [0b01100, 0b10010, 0b10010, 0b10110, 0b01110],  # Q
-            [0b11100, 0b10010, 0b11100, 0b10100, 0b10010],  # R
-            [0b01110, 0b10000, 0b01100, 0b00010, 0b11100],  # S
-            [0b11110, 0b00100, 0b00100, 0b00100, 0b00100],  # T
-            [0b10010, 0b10010, 0b10010, 0b10010, 0b01100],  # U
-            [0b10010, 0b10010, 0b10010, 0b01100, 0b00100],  # V
-            [0b10010, 0b10010, 0b10110, 0b11010, 0b10010],  # W
-            [0b10010, 0b01100, 0b00100, 0b01100, 0b10010],  # X
-            [0b10010, 0b10010, 0b01100, 0b00100, 0b00100],  # Y
-            [0b11110, 0b00010, 0b00100, 0b01000, 0b11110],  # Z
-            [0b01100, 0b10010, 0b10010, 0b10010, 0b01100],  # 0
-            [0b00100, 0b01100, 0b00100, 0b00100, 0b01110],  # 1
-            [0b01100, 0b10010, 0b00100, 0b01000, 0b11110],  # 2
-            [0b01100, 0b10010, 0b00100, 0b10010, 0b01100],  # 3
-            [0b10010, 0b10010, 0b11110, 0b00010, 0b00010],  # 4
-            [0b11110, 0b10000, 0b11100, 0b00010, 0b11100],  # 5
-            [0b01100, 0b10000, 0b11100, 0b10010, 0b01100],  # 6
-            [0b11110, 0b00010, 0b00100, 0b01000, 0b10000],  # 7
-            [0b01100, 0b10010, 0b01100, 0b10010, 0b01100],  # 8
-            [0b01100, 0b10010, 0b01110, 0b00010, 0b01100]   # 9
-        ]
-        for i, char in enumerate(text):
-            if char == ' ':
-                continue
-            elif char.isdigit():
-                offset = 26 + int(char)
-            else:
-                offset = ord(char.upper()) - ord('A')
-            if 0 <= offset < len(font):
-                for dy, row in enumerate(font[offset]):
-                    for dx in range(5):
-                        if row & (1 << (4 - dx)):
-                            pyxel.pset(x + i * 6 + dx, y + dy, col)
+            pyxel.text(40,25,"GAME OVER", self.pal[2])
 
     # 縦書きテキスト描画（「FUEL」用）
     def draw_vertical_text(self, x, y, text, col):
-        font = [
-            [0b01100, 0b10010, 0b11110, 0b10010, 0b10010],  # A
-            [0b11100, 0b10010, 0b11100, 0b10010, 0b11100],  # B
-            [0b01100, 0b10000, 0b10000, 0b10000, 0b01100],  # C
-            [0b11100, 0b10010, 0b10010, 0b10010, 0b11100],  # D
-            [0b11110, 0b10000, 0b11100, 0b10000, 0b11110],  # E
-            [0b11110, 0b10000, 0b11100, 0b10000, 0b10000],  # F
-            [0b01100, 0b10000, 0b10110, 0b10010, 0b01100],  # G
-            [0b10010, 0b10010, 0b11110, 0b10010, 0b10010],  # H
-            [0b11110, 0b00100, 0b00100, 0b00100, 0b11110],  # I
-            [0b00110, 0b00010, 0b00010, 0b10010, 0b01100],  # J
-            [0b10010, 0b10100, 0b11000, 0b10100, 0b10010],  # K
-            [0b10000, 0b10000, 0b10000, 0b10000, 0b11110],  # L
-            [0b10010, 0b11010, 0b10110, 0b10010, 0b10010],  # M
-            [0b10010, 0b11010, 0b10110, 0b10010, 0b10010],  # N
-            [0b01100, 0b10010, 0b10010, 0b10010, 0b01100],  # O
-            [0b11100, 0b10010, 0b11100, 0b10000, 0b10000],  # P
-            [0b01100, 0b10010, 0b10010, 0b10110, 0b01110],  # Q
-            [0b11100, 0b10010, 0b11100, 0b10100, 0b10010],  # R
-            [0b01110, 0b10000, 0b01100, 0b00010, 0b11100],  # S
-            [0b11110, 0b00100, 0b00100, 0b00100, 0b00100],  # T
-            [0b10010, 0b10010, 0b10010, 0b10010, 0b01100],  # U
-            [0b10010, 0b10010, 0b10010, 0b01100, 0b00100],  # V
-            [0b10010, 0b10010, 0b10110, 0b11010, 0b10010],  # W
-            [0b10010, 0b01100, 0b00100, 0b01100, 0b10010],  # X
-            [0b10010, 0b10010, 0b01100, 0b00100, 0b00100],  # Y
-            [0b11110, 0b00010, 0b00100, 0b01000, 0b11110],  # Z
-            [0b01100, 0b10010, 0b10010, 0b10010, 0b01100],  # 0
-            [0b00100, 0b01100, 0b00100, 0b00100, 0b01110],  # 1
-            [0b01100, 0b10010, 0b00100, 0b01000, 0b11110],  # 2
-            [0b01100, 0b10010, 0b00100, 0b10010, 0b01100],  # 3
-            [0b10010, 0b10010, 0b11110, 0b00010, 0b00010],  # 4
-            [0b11110, 0b10000, 0b11100, 0b00010, 0b11100],  # 5
-            [0b01100, 0b10000, 0b11100, 0b10010, 0b01100],  # 6
-            [0b11110, 0b00010, 0b00100, 0b01000, 0b10000],  # 7
-            [0b01100, 0b10010, 0b01100, 0b10010, 0b01100],  # 8
-            [0b01100, 0b10010, 0b01110, 0b00010, 0b01100]   # 9
-        ]
         for i, char in enumerate(text.upper()):
-            if char == ' ':
-                continue
-            elif char.isdigit():
-                offset = 26 + int(char)
-            else:
-                offset = ord(char) - ord('A')
-            if 0 <= offset < len(font):
-                for dy, row in enumerate(font[offset]):
-                    for dx in range(5):
-                        if row & (1 << (4 - dx)):
-                            pyxel.pset(x + dx, y + i * 6 + dy, col)
+            pyxel.text(x, y+i*6, char, col)
 
 # ゲーム開始
 LunarLander()
+
+# End of lander.py
